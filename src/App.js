7aed0e98f3c82a0c21d288237f3a1397f5f42743
Vikas Gorur <vikasgp@gmail.com>
2016-10-23 20:25:53 +0530

    Add a simple compose box

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Compose from './Compose';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+            <Compose />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/Compose.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import TextField from 'material-ui/TextField';
+
+export default class Compose extends Component {
+  style = {
+    border: "solid 2px blue",
+    borderRadius: "10px",
+    padding: "10px"
+  }
+
+  render() {
+    return <TextField hintText="Write your tweet"
+                      multiLine={true}
+                      rows={4} 
+                      style={this.style} />;
+  }
+}
