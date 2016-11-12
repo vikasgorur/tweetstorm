d090bd05c3d393a0a10b4fa13a2a3d9c2b74308d
Vikas Gorur <vikasgp@gmail.com>
2016-11-12 21:55:38 +0530

    Get a basic version working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import './css/skeleton.css';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    text: ''
+  },
+
+  getters: {
+    tweets: state => {
+      return state.text.split(',')
+    }
+  },
+
+  mutations: {
+    setText(state, text) {
+      state.text = text;
+    }
+  }
+});
+
+Vue.component('compose', {
+  template: '#compose-template',
+
+  data: function() {
+    return {
+      text: ''
+    }
+  },
+
+  methods: {
+    handleChange: function(event) {
+      this.$store.commit('setText', this.text);
+    }
+  }
+});
+
+Vue.component('tweets', {
+  template: '#tweets-template',
+
+  computed: {
+    tweets() {
+      return this.$store.getters.tweets;
+    }
+  }
+});
+
+new Vue({
+  el: '#root',
+  store
+});
