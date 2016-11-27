6af4ae73a0ad61ac2ce1cbe55c68ef33db6d29f8
Vikas Gorur <vikasgp@gmail.com>
2016-11-27 15:52:54 +0530

    Show tweet length

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ const helpText = `This is a simple app for composing tweetstorms.
 You can compose your text here, separating them with '--'.
 --
 You can click on any of the tweets on the right to copy them to the clipboard.
+--
+If a tweet is very long, like this one is, the length tells you that it is very long,
+and that you should do something about it, like maybe making it shorter than it is.
 `;
 
 const store = new Vuex.Store({
