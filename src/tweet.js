b3e533c1031694acd4cb3043a3f5f82eaf49f5e3
Vikas Gorur <vikasgp@gmail.com>
2016-11-27 15:14:17 +0530

    Show tweet length

--- src/tweet.js
+++ src/tweet.js
@@ -1,8 +1,15 @@
-//import twitter from 'twitter-text';
+import twitter from 'twitter-text';
 
 /**
  * Split the text into chunks based on '\n--\n'
  */
 export function split(text) {
-  return text.split('\n--\n');
+  let pieces = text.split('\n--\n');
+  let tweets = [];
+
+  for (const t of pieces) {
+    tweets.push({ text: t, length: twitter.getTweetLength(t) });
+  }
+
+  return tweets;
 }
